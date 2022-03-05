a047b5d9ab2c8f723abc786b0b36353edcef3c48
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-03-05 17:16:00 +0000

    Html code clean-up, CSS media query added for larger screens and Javascript for To-do-list functionality

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Create a "close" button and append it to each list item
+
+const listContainer = document.createElement('ul'); // grap list container
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+
+
+listContainer.addEventListener('click', function(){
+    let textEl = document.createElement('text');
+    textEl.classList.add('text-styling');
+    textEl.innerText = inputEl.value;
+    listContainer.appendChild(textEl);
+    inputEl.value = "";
+    textEl.addEventListener('click', function(){
+        textEl.style.textDecoration = "line-through";
+    })
+    textEl.addEventListener('dblclick', function(){
+        listContainer.removeChild(textEl);
+    })
+})
