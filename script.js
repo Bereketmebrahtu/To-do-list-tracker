69bca9dd32c720d1989f008147c81a3ed9a72f51
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-03-05 17:44:38 +0000

    Chaged variable names

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // Create a "close" button and append it to each list item
 
-const listContainer = document.createElement('ul'); // grap list container
+const listContainer = document.querySelector('ul'); // grap list container
 const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 
 
-listContainer.addEventListener('click', function(){
-    let textEl = document.createElement('text');
-    textEl.classList.add('text-styling');
-    textEl.innerText = inputEl.value;
-    listContainer.appendChild(textEl);
+listContainer.addEventListener('click', function() {
+    let listItems = document.createElement('li');
+    listItems.classList.add('text-styling');
+    listItems.innerText = inputEl.value;
+    listContainer.appendChild(listItems);
     inputEl.value = "";
-    textEl.addEventListener('click', function(){
-        textEl.style.textDecoration = "line-through";
+    listItems.addEventListener('click', function(){
+        listItems.style.textDecoration = "line-through";
     })
-    textEl.addEventListener('dblclick', function(){
-        listContainer.removeChild(textEl);
+    listItems.addEventListener('dblclick', function(){
+        listContainer.removeChild(listItems);
     })
 })
+
