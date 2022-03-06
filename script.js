27666339a666525f5c9616511186be53ec06229c
artgalla8 <artgalla8@gmail.com>
2022-03-06 21:29:11 +0000

    function add-tho-the-list

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//Create a new list item when clicking on the "Add" button
+function addToList() {
+   const input =document.getElementById("input").value;
+   const node = document.createElement("li");
+   const textnode = document.createTextNode(input);
+   if (input === '') {
+      alert("You must write something!");
+   } else {
+   node.appendChild(textnode);
+  document.getElementById("list").appendChild(node);
+   }
+}
+
+
+
+
+
 
 // Create a "close" button and append it to each list item
 
