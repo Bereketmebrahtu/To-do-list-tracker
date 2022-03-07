fbdf8e07646c79cc5db35003831b3179859b665a
artgalla8 <artgalla8@gmail.com>
2022-03-07 21:14:56 +0000

    back to previous

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ function addToList() {
    }
 }
 
+// Add a "checked" symbol when clicking on a list item
+const list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+  ev.target.classList.toggle('checked');
+  }
+}, false);
 
 
 
