0e63aded1d89c2647af035a70cf88874fd80954c
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-03-07 20:23:04 +0000

    Function added with event listener and button

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-//Create a new list item when clicking on the "Add" button
+const buttonEl = document.querySelector('button');
+
+
+buttonEl.addEventListener('click', addToList)
+
 function addToList() {
-   const input =document.getElementById("input").value;
-   const node = document.createElement("li");
-   const textnode = document.createTextNode(input);
-   if (input === '') {
-      alert("You must write something!");
-   } else {
-   node.appendChild(textnode);
-  document.getElementById("list").appendChild(node);
-   }
-}
+
+    const input =document.getElementById("input").value;
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(input);
+    if (input === '') {
+       alert("You must write something!");
+    } else {
+    node.appendChild(textnode);
+   document.getElementById("list").appendChild(node);
+   
+    }
+ }
+ 
 
 
 
