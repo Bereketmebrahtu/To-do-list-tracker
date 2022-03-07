7ce008e1f53ad07db093ae93219e7e34e2dfabda
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-03-07 20:26:35 +0000

    integration Test added

--- index.test.js
+++ index.test.js
@@ -1,6 +1,14 @@
-test("Submitting a new task adds it to the list", () => {
-  // test goes here
-});
+
+test("Submitting a new task adds it to the list", addToList => {
+    const input = document.querySelector("input"); // step 1
+    input.value = "test"; // step 2
+    const spanEl = document.querySelector("button");
+    spanEl.click(); // step 3
+    const textnode = document.querySelector("input");
+    equal(textnode.textContent, "TEST"); // step 4
+    textnode.textContent = ""; // reset the page so it doesn't affect the page/other tests
+  });
+
 
 test("Checking an entry marks it as complete", () => {
   // test goes here
