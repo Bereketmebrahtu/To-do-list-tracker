534ce89a0c9f8c154c785ad03d2986e3e46d5110
artgalla8 <artgalla8@gmail.com>
2022-03-08 09:48:25 +0000

    all the javascript done and bg image added

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
-//Create a new list item when clicking on the "Add" button
-function addToList() {
-   const input =document.getElementById("input").value;
-   const node = document.createElement("li");
-   const textnode = document.createTextNode(input);
-   if (input === '') {
-      alert("You must write something!");
-   } else {
-   node.appendChild(textnode);
-  document.getElementById("list").appendChild(node);
-   }
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  const li = document.createElement("li");
+  const inputValue = document.getElementById("input").value;
+  const t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("input").value = "";
+
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      const div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
 
 // Add a "checked" symbol when clicking on a list item
 const list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
-  ev.target.classList.toggle('checked');
+    ev.target.classList.toggle('checked');
   }
 }, false);
 
 
-
-
-
+// Click on a close button to hide the current list item
+const close = document.getElementsByClassName("close");
+let i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
