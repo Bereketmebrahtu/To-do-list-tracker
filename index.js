84903bada27c97ae10b99163c698e454d7864261
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-03-09 16:44:28 +0000

    Minor tweaks to add functionality and implement test

---
+++ index.js
@@ -0,0 +1,67 @@
+const buttonEl = document.querySelector('.Btn');
+
+
+buttonEl.addEventListener('click', addToList)
+
+function addToList() {
+
+    const input =document.getElementById("input").value;
+   
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(input);
+    
+    if (input === '') {
+       alert("You must write something!");
+    } else {
+    node.appendChild(textnode);
+   document.getElementById("list").appendChild(node);
+   
+    }
+ }
+ 
+ // Add a "checked" symbol when clicking on a list item
+ function addCheckedSymbol() {
+   let list = document.querySelector('ul');
+   const listItem = document.querySelector('li')
+   console.log(listItem)
+   const node = document.createElement("li");
+   console.log(node)
+   node.className = 'checked'
+   list.addEventListener('click', ev => {
+     if (ev.target. === 'LI') {
+       ev.target.classList.toggle('checked');
+       
+     }
+   }, false);
+   
+
+ }
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("input").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  let buttonEl = document.createElement("button");
+  let txt = document.createTextNode("text");
+  buttonEl.className = "close";
+  buttonEl.appendChild(txt);
+  li.appendChild(buttonEl);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+
--- index.test.js
+++ index.test.js
@@ -1,11 +1,61 @@
-test("Submitting a new task adds it to the list", () => {
-  // test goes here
-});
 
-test("Checking an entry marks it as complete", () => {
-  // test goes here
-});
+  test("Submitting a new task adds it to the list", () => {
+    const input = document.getElementById("input"); // step 1
+   
+    input.value = "test"; // step 2
+   
+    // input.value.toUpperCase()
+    const inputEl = document.querySelector("input");
+    inputEl.click(); // step 3
+   
+    const result = document.querySelector("#result");
+  
+    
+    const value = input.value; // get the value of the name="text" input
+  
+    
+    const uppercased = input.value.toUpperCase();
+  
+   
+  // uppercase the value
+    result.textContent = uppercased;
+    
+   // step 4
+    result.textContent = uppercased; // reset the page so it doesn't affect the page/other tests
+    equal(result.textContent, "TEST"); 
+  });
+
+  
+
+test("Checking a list item and marks it as complete", () => {
+
+   
+    const listItem = document.querySelector('div').style.backgroundColor = 'RED'
+//     const node = document.createElement("li");
+ 
+   
+    
+    let list = document.querySelector('ul');
+    list.click(); // step 3
+   
+    const result = document.querySelector("#result")
+     const listItemTextDecorationChanged = listItem
+    result.textContent = listItemTextDecorationChanged
+        equal(result.textContent, "RED" ); 
+  
+
+    list.addEventListener('click', ev => {
+       if (ev.target.tagName === 'LI') {
+         ev.target.classList.toggle('checked');
+        
+      }
+    }, false);
+ });
 
 test("Deleting an entry removes it from the list", () => {
   
 });
+
+// // function equal(textContent, arg1) {
+// //   throw new Error("Function not implemented.");
+// // }
