87b5febbcc0fa1370a5ae71ca78eb7dec93fbb51
artgalla8 <artgalla8@gmail.com>
2022-03-09 16:19:47 +0000

    small changes

--- index.test.js
+++ index.test.js
@@ -1,11 +1,29 @@
 test("Submitting a new task adds it to the list", () => {
-  // test goes here
+
+  //get the input
+  const myInput = document.querySelector("input");
+  // enter test data into inputs
+  myInput.value = "Have some coffee";
+  // submit the form
+  const submitButton = document.querySelector("span");
+  submitButton.click();
+   // verify that the page contains the expected result
+   const result = document.querySelector("li");
+   equal(result.textContent, "Have some coffee");
+   // reset the page so it doesn't affect anything else
+   result.textContent = ""; 
+
+
 });
 
 test("Checking an entry marks it as complete", () => {
-  // test goes here
+//get the input
+// reset the page so it doesn't affect anything else
+result.textContent = ""; 
 });
 
 test("Deleting an entry removes it from the list", () => {
-  
+//get the input
+// reset the page so it doesn't affect anything else
+result.textContent = ""; 
 });
--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  const li = document.createElement("li");
-  const inputValue = document.getElementById("input").value;
-  const t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("list").appendChild(li);
-  }
-  document.getElementById("input").value = "";
+    // Create an "li" node:
+    const node = document.createElement("li");
+    const inputValue = document.getElementById("input").value;
+    // Create a text node:
+    const textnode = document.createTextNode(inputValue);
+    // Append the text node to the "li" node:
+    node.appendChild(textnode);
+    // Append the "li" node to the list:
+    document.getElementById("list").appendChild(node);
+  
 
   const span = document.createElement("SPAN");
   const txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
-  li.appendChild(span);
+  node.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
